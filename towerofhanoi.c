7c35a1918af211729c5efd9a9ca613de35f1bf1e
Fael <53711123+rafaelmelo2@users.noreply.github.com>
2022-11-23 10:09:05 -0300

    Create towerofhanoi.c

---
+++ towerofhanoi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int discos;
+    printf("\t\t\t\tTOWER OF HANOI\n\n");
+    printf("\nEnter the number of disks: ");
+    scanf("%d", &disks);
+    movetorre(disks,'A', 'C', 'B');
+    return 0;
+
+}
+
+void movetower(int n, char x, char y, char z){
+    int i;
+    if(n==1){
+        printf("\nDisk 1 passed the tower %c to tower %c", x, y);
+        return;
+    }
+    movetower(n-1, x, z, y);
+    printf("\nDisk %d passed the tower %c to tower %c", n, x, y);
+    movetower(n-1, z, y, x);
+    for(i=0; i<n; i++)
+        printf("\n\tDisk %d stayed in the tower %c", i+1, y);
+}
